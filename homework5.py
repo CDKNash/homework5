c76b0c99dabb4719ea0e651c1133640ee95b6fda
CDKNash <cdknash123@gmail.com>
2024-06-30 16:47:55 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = ('list', 'pen', 'pencil', 55)
+print(immutable_var)
+#immutable_var[0] = notebook
+#print(immutable_var)
+mutable_list = ['list', 'pen', 'pencil', 55]
+mutable_list.append(2024)
+print(mutable_list)
